a8515012a5b2e7e48640c0adddcc4313ff4d5cb0
Swopnil-01 <bhattaswopnil@gmail.com>
2025-12-31 20:03:27 +0545

    Menu

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-const totalTables = 15;
-const menuURL = "https://your-menu-link.com";
+const menuURL = "https://swopnil-01.github.io/vhunte-bamboo-menu/";
 
-const container = document.getElementById("tables");
+document.getElementById("download").addEventListener("click", () => {
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF('p', 'pt', 'a4');
 
-for (let i = 1; i <= totalTables; i++) {
-
-    const card = document.createElement("div");
-    card.className = "card";
-
-    card.innerHTML = `
-        <h2>VHUNTE BAMBOO COTTAGE</h2>
-        <h3>TABLE ${String(i).padStart(2, "0")}</h3>
-        <div id="qr${i}" class="qr"></div>
-        <div class="note">Scan for Menu</div>
-    `;
-
-    container.appendChild(card);
-
-    new QRCode(document.getElementById(`qr${i}`), {
-        text: menuURL,
-        width: 100,
-        height: 100,
-        colorDark: "#000000",
-        colorLight: "#ffffff"
+    doc.html(document.body, {
+        callback: function(doc) {
+            doc.save("Cafe_Menu.pdf");
+        },
+        x: 20,
+        y: 20,
+        html2canvas: { scale: 0.57 }
     });
-}
+});
